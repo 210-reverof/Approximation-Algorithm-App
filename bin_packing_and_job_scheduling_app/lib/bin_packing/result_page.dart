2f92eb431f561603591c47c296959afa4dce5896
210_reverof <sheltonwon@gmail.com>
2022-04-24 21:46:24 +0900

    conplete binpacking box visiualation

--- bin_packing_and_job_scheduling_app/lib/bin_packing/result_page.dart
+++ bin_packing_and_job_scheduling_app/lib/bin_packing/result_page.dart
@@ -9,10 +9,14 @@ class ResultPage extends StatefulWidget {
   State<ResultPage> createState() => _ResultPageState();
 }
 
+int typeNum = 0;
+
 class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
-    print(widget.inputList.toString());
+    //print("input : " + widget.inputList.toString());
+    double boxWidth = MediaQuery.of(context).size.width - 50;
+    double boxHeight = MediaQuery.of(context).size.height - 250;
 
     return Scaffold(
         resizeToAvoidBottomInset: false,
@@ -21,9 +25,101 @@ class _ResultPageState extends State<ResultPage> {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [getResultBox(widget.inputList, widget.binSize)],
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              getTypeButton(0, "처음\n적합"),
+              getTypeButton(1, "다음\n적합"),
+              getTypeButton(2, "최선\n적합"),
+              getTypeButton(3, "최악\n적합")
+            ]),
+            SizedBox(height: 10),
+            getResultBox(widget.inputList, widget.binSize, boxWidth, boxHeight)
+          ],
         ));
   }
+
+  Widget getTypeButton(int num, String text) {
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              typeNum = num;
+            });
+          },
+          child: Text(text)),
+    );
+  }
+}
+
+Widget getResultBox(List inputList, int binSize, double width, double height) {
+  List<List<int>> resultList = typeNum == 0
+      ? getFirstFitResult(inputList, binSize)
+      : typeNum == 1
+          ? getNextFitResult(inputList, binSize)
+          : typeNum == 2
+              ? getBestFitResult(inputList, binSize)
+              : getWorstFitResult(inputList, binSize);
+
+  // for debugging
+  String str = "";
+
+  for (int i = 0; i < resultList.length; i++) {
+    str = str + " " + resultList[i].toString();
+  }
+  print("values : " + str);
+//////////////////////////////////
+
+  List<Widget> boxes = [];
+
+  // scale 구하기
+  double binWidthScale =
+      (width / (4 * (resultList.length) - 1)); // 3칸은 한 가로 넓이, 1칸은 여백
+  double binHeightScale = height / binSize;
+
+  // 각 사각형 넣기
+  for (int i = 0; i < resultList.length; i++) {
+    boxes.add(Positioned(
+        bottom: 0,
+        left: i * binWidthScale * 4,
+        child: Container(
+          width: binWidthScale * 3,
+          height: binHeightScale * binSize,
+          color: Color.fromARGB(255, 201, 201, 201),
+        )));
+  }
+
+  // 각 요소 사각형
+  for (int i = 0; i < resultList.length; i++) {
+    double top = 0;
+    for (int j = 0; j < resultList[i].length; j++) {
+      boxes.add(Positioned(
+          bottom: top,
+          left: i * binWidthScale * 4,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 131, 148, 202),
+                border: Border.all(color: Color.fromARGB(255, 7, 10, 15))),
+            width: binWidthScale * 3,
+            height: binHeightScale * resultList[i][j],
+            child: Center(
+              child: Text(resultList[i][j].toString()),
+            ),
+          )));
+      top += binHeightScale * resultList[i][j];
+    }
+  }
+  return Center(
+    child: Container(
+      color: Colors.white,
+      width: width,
+      height: height,
+      child: Stack(
+        children: boxes,
+      ),
+    ),
+  );
 }
 
 List<List<int>> getFirstFitResult(List inputList, int binSize) {
@@ -146,14 +242,4 @@ List<List<int>> getWorstFitResult(List inputList, int binSize) {
   }
 
   return outputList;
-}
-
-Widget getResultBox(List inputList, int binSize) {
-  List<List<int>> outputList = getNextFitResult(inputList, binSize);
-  String str = "";
-
-  for (int i = 0; i < outputList.length; i++) {
-    str = str + " " + outputList[i].toString();
-  }
-  return Container(color: Colors.amber, child: Text(str));
 }
